1c5924b158ee73fb5f00c72a7511b2ce8c18e319
Naresh N <nareshn141819@gmail.com>
2026-03-30 14:37:45 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ import os, re, requests
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import firebase_admin
-from firebase_admin import credentials, auth as firebase_auth
+from firebase_admin import credentials, auth as firebase_auth 
 
 cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
